6326a923e57212fbe43a61e5312f6eee8b3d0225
evan <evan.hermier@gmail.com>
2021-11-04 22:20:26 +0100

    get and console.log data

---
+++ frontend/src/components/IcebathList.vue
@@ -0,0 +1,39 @@
+<template>
+  <p>hi</p>
+</template>
+
+<script>
+import IcebathDataService from "./../services/IcebathDataService";
+
+export default {
+  name: "ice-bath-list",
+  data() {
+    return {
+      icebaths: [],
+    };
+  },
+  methods: {
+    retrieveIcebath() {
+      IcebathDataService.getAll()
+        .then((response) => {
+          this.icebaths = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() {
+    this.retrieveIcebath();
+  },
+};
+</script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
