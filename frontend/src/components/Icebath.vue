5c6c1c1b9665ac4758e0c227410bcdb4da38d97c
evan <evan.hermier@gmail.com>
2021-11-07 20:47:41 +0100

    edit card by id style

--- frontend/src/components/Icebath.vue
+++ frontend/src/components/Icebath.vue
@@ -1,25 +1,57 @@
 <template>
   <div class="container mt-3">
     <div class="row">
-      <ul class="col-12 col-md-6 my-5 mx-auto">
-        <li>Participants: {{ currentIcebath.firstName }}</li>
-        <li>Date: {{ currentIcebath.createdAt }}</li>
-        <li>Ressenti global: {{ currentIcebath.globalFeeling }}</li>
-        <li>
-          Sport avant la baignade: {{ currentIcebath.physicalActivityBefore }}
+      <ul class="col-12 col-md-6 my-5 mx-auto list-group">
+        <li class="list-group-item text-left">
+          🛀 Participants: {{ currentIcebath.firstName }}
         </li>
-        <li>Temps de récupération: {{ currentIcebath.recoveryTime }}</li>
-        <li>
-          Stress pendant la récupération{{
-            currentIcebath.stressDuringRecovery
-          }}
+        <li class="list-group-item text-left">
+          📅 Date: {{ currentIcebath.createdAt }}
         </li>
-        <li>Temperature exterieur: {{ currentIcebath.temperatureOutside }}</li>
-        <li>Temps resté dans l'eau: {{ currentIcebath.timeInWater }}</li>
-        <li>Temperature de l'eau: {{ currentIcebath.waterTemperature }}</li>
-        <li>Météo: {{ currentIcebath.weather }}</li>
-        <li>Vent: {{ currentIcebath.wind }}</li>
-        <li>Commentaires: {{ currentIcebath.commentary }}</li>
+        <li class="list-group-item text-left">
+          🙄 Ressenti global: {{ currentIcebath.globalFeeling }}
+        </li>
+        <li class="list-group-item text-left">
+          🏋️‍♂️ Sport avant la baignade:
+          {{ currentIcebath.physicalActivityBefore }}
+        </li>
+        <li class="list-group-item text-left">
+          ⌛ Temps de récupération:
+          <span class="badge badge-primary badge-pill">{{
+            currentIcebath.recoveryTime
+          }}</span>
+        </li>
+        <li class="list-group-item text-left">
+          😅 Stress pendant la récupération:
+          {{ currentIcebath.stressDuringRecovery }}
+        </li>
+        <li class="list-group-item text-left">
+          🌡 Temperature exterieur:
+          <span class="badge badge-primary badge-pill">{{
+            currentIcebath.temperatureOutside
+          }}</span>
+        </li>
+        <li class="list-group-item text-left">
+          💧 Temps resté dans l'eau:
+          <span class="badge badge-primary badge-pill">{{
+            currentIcebath.timeInWater
+          }}</span>
+        </li>
+        <li class="list-group-item text-left">
+          🌀 Temperature de l'eau:
+          <span class="badge badge-primary badge-pill">{{
+            currentIcebath.waterTemperature
+          }}</span>
+        </li>
+        <li class="list-group-item text-left">
+          🌦 Météo: {{ currentIcebath.weather }}
+        </li>
+        <li class="list-group-item text-left">
+          🌬 Vent: {{ currentIcebath.wind }}
+        </li>
+        <li class="list-group-item text-left">
+          🤠 Commentaires: {{ currentIcebath.commentary }}
+        </li>
       </ul>
     </div>
   </div>
